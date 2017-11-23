ca7b200a0af235eb4d1c9cc9f8219b718aab28c6
Guilherme Sehn <ghsehn@gmail.com>
2017-11-23 02:33:14 -0200

    here we go

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # simula_image_extractor
+
 Extrai imagens de agentes de projetos do SIMULA (.dat)
+
+## Uso
+
+```
+$ npm install
+$ node script.js examples/transmissao_doencas.dat
+
+Extracting pessoa_saudavel...
+pessoa_saudavel finished.
+Extracting pessoa_baixa_imunidade...
+pessoa_baixa_imunidade finished.
+Extracting medico...
+medico finished.
+Extracting parede...
+parede finished.
+Extracting pessoa_infectada_permanente_sem_sintomas...
+pessoa_infectada_permanente_sem_sintomas finished.
+Extracting pessoa_infectada...
+pessoa_infectada finished.
+Extracting pessoa_em_tratamento...
+pessoa_em_tratamento finished.
+Extracting pessoa_infectada_sem_sintomas...
+pessoa_infectada_sem_sintomas finished.
+
+Result available on /Users/guilherme/Desktop/simula_image_extractor/results/transmissao_doencas.dat/
+```
---
+++ script.js
@@ -0,0 +1,95 @@
+const fs = require('fs')
+const rmdir = require('rimraf') // rm -rf
+const Jimp = require('jimp')
+
+let args = process.argv.slice(2)
+let fileName = args[0]
+let resultFolderPath = 'results/' + fileName.split('/').pop()
+
+const IMAGE_WIDTH = 16
+const IMAGE_HEIGHT = 16
+
+const AGENT_NAME_INDEX = 3
+const IMAGE_START_INDEX = 8
+const IMAGE_END_INDEX = IMAGE_START_INDEX + (IMAGE_WIDTH * IMAGE_HEIGHT) - 1
+
+if (!fileName || fileName.trim() === '') {
+	throw new Error('Invalid file name')
+}
+
+if (!fs.existsSync(fileName)) {
+  throw new Error('File not found')
+}
+
+if (fs.existsSync(resultFolderPath)) {
+  rmdir.sync(resultFolderPath)
+}
+
+if (!fs.existsSync('results')) {
+  fs.mkdirSync('results')
+}
+
+fs.mkdirSync(resultFolderPath)
+
+let lines = fs.readFileSync(fileName).toString().split('\n')
+let agentLineIndex = -1
+let agentName = null
+let agentImage = null
+let imageX, imageY
+
+lines.forEach(line => {
+  line = line.trim()
+
+  if (line === '.A.') {
+    agentLineIndex = 0
+    agentImage = new Jimp(IMAGE_WIDTH, IMAGE_HEIGHT)
+  }
+
+  if (agentLineIndex !== -1) {
+    agentLineIndex++
+
+    if (agentLineIndex === AGENT_NAME_INDEX) {
+      // skip agents with no name
+      if (line === '') {
+        agentLineIndex = -1
+        return
+      }
+
+      agentName = line
+      imageX = 0
+      imageY = 0
+
+      console.log(`Extracting ${agentName}...`)
+    }
+
+    if (agentLineIndex >= IMAGE_START_INDEX) {
+      let color = parseInt(line, 10)
+      color = color & 0xffffff
+      color = color.toString(16) + 'ff'
+
+      if (color === 'ffffffff') color = 'ffffff00' // white -> transparent
+
+      color = parseInt(color, 16)
+
+      agentImage.setPixelColor(color, imageX, imageY)
+
+      imageX++
+
+      if (imageX === 16) {
+        imageX = 0
+        imageY++
+      }
+    }
+
+    if (agentLineIndex === IMAGE_END_INDEX) {
+      agentLineIndex = -1
+
+      agentImage.write(`${resultFolderPath}/${agentName}.png`)
+
+      console.log(`${agentName} finished.`)
+    }
+  }
+})
+
+console.log()
+console.log(`Result available on ${__dirname}/${resultFolderPath}/`)
